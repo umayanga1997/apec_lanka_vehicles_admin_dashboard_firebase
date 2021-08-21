ebe3379fda67fad47da3e25e5f2221f3048a0166
umayanga avi <umayanga.avi@gmail.com>
2021-08-21 21:19:16 +0530

    image update

--- src/background.js
+++ src/background.js
@@ -43,7 +43,7 @@ async function createWindow() {
     } else {
         createProtocol('app')
             // Load the index.html when not in development
-        win.loadURL('app://./index.html')
+        win.loadURL('app://index.html')
             // window.loadURL(formatUrl({
             //     pathname: path.join(__dirname, 'index.html'),
             //     protocol: 'file',
